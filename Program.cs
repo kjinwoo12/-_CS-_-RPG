ced577d50d006a50e21626af19d38bfebeaba8a4
Kim JinWoo <kjinwoo12@gmail.com>
2024-09-26 19:11:19 +0900

    Add dot-art for hotelScene

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
         IScene currentScene = new CreateCharacterScene();
         while (true)
         {
