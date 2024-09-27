88f1b8cb3671833ea96c4e8e64a96fef43099775
pchanbro <cksgud0509@naver.com>
2024-09-27 19:25:50 +0900

    feat : 공격 함수 추가, 데미지받기 함수 추가
    공격 타입에 따른 치명타 공격, 공격 여부 결정,
    공격자에의 공격 결과에 따른 데미지 받기 결정

--- Character/Character.cs
+++ Character/Character.cs
@@ -1,6 +1,41 @@
 ﻿using System;
 using System.Threading;
 
+public enum AttackType
+{
+    Normal = 0,
+    Weak,
+    Critical
+}
+
+public struct AttackResult
+{
+    public Character attacker = null;
+    public Character target = null;
+    public AttackType attackType;
+    public int damage;
+
+    public AttackResult()
+    {
+        attackType = AttackType.Normal;
+        damage = 0;
+    }
+}
+
+public struct TakeDamageResult
+{
+    public AttackResult attackResult = new AttackResult();
+    public int actualDamage;
+    public int defenseAmount;
+
+    public TakeDamageResult()
+    {
+        actualDamage = 0;
+        defenseAmount = 0;
+    }
+}
+
+
 public class Character
 {
     public int level { get; protected set; } = 1;
@@ -19,10 +54,6 @@ public class Character
     public int health { get; set; }
 
     public bool isDead => health <= 0;
-    public int attack => 
-        new Random().NextDouble() < stats.criticalRate ? 
-        new Random().Next(stats.minAttack * 2, stats.maxAttack * 2 + 1) :
-        new Random().Next(stats.minAttack, stats.maxAttack + 1);
 
     public Character(string name, CharacterStats originalStats)
     {
@@ -33,27 +64,38 @@ public class Character
         equipmentComponent.OnUnequipDelegate += OnUnequip;
     }
 
-    public void takeDamage(int damage)
+    public AttackResult DoAttack(Character target)
     {
-        Random rnd = new Random();
+        AttackResult attackResult = new AttackResult();
+        attackResult.attacker = this;
+        attackResult.target = target;
 
-        damage -= rnd.Next(stats.minArmor, stats.maxArmor + 1);
-        if (damage <= 0)
+        if(new Random().NextDouble() < this.stats.criticalRate)
         {
-            damage = 1;
-        }
-
-        health -= damage;
-        Console.WriteLine($"{name}이(가) {damage}의 데미지를 받았습니다");
-        Thread.Sleep(500);
-        if (isDead)
-        {
-            Console.WriteLine($"{name}이(가) 죽었습니다.");
+            attackResult.attackType = AttackType.Critical;
+            attackResult.damage = new Random().Next(this.stats.minAttack * 2, this.stats.maxAttack * 2 + 1);
         }
         else
         {
-            Console.WriteLine($"{name}의 남은 체력: {health}/{stats.maxHealth}");
+            attackResult.attackType = AttackType.Normal;
+            attackResult.damage = new Random().Next(this.stats.minAttack, this.stats.maxAttack + 1);
         }
+
+        return attackResult;
+    }
+
+    public TakeDamageResult takeDamage(AttackResult attackResult)
+    {
+        TakeDamageResult result = new TakeDamageResult();
+        result.attackResult = attackResult;
+        result.defenseAmount = (new Random().Next(this.stats.minArmor, this.stats.maxArmor + 1));
+        result.actualDamage = attackResult.damage - result.defenseAmount;
+        if (result.actualDamage <= 0)
+        {
+            result.actualDamage = 1;
+        }
+
+        return result;
     }
 
     void OnEquip(IEquipableItem equipableItem)
