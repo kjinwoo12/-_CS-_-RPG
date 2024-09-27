864719a68f3c1fe40b4ed07678f52a7f02f89466
dearnm <minbongee@gmail.com>
2024-09-27 11:04:11 +0900

    Add title Ascii art

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program
         IScene currentScene = new CreateCharacterScene();
         while (true)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.Clear();
             currentScene.OnShow();
             currentScene = currentScene.GetNextScene();
--- Scene/CreateCharacterScene.cs
+++ Scene/CreateCharacterScene.cs
@@ -4,12 +4,32 @@ public class CreateCharacterScene : IScene
 {
     public void OnShow()
     {
-        Console.WriteLine("원하시는 캐릭터의 이름을 설정해주세요.");
-        Console.Write(">> ");
+        Console.WriteLine("        _________  ___  _________  ________  ________   ___  ________ ");
+        Console.WriteLine("       |\\___   ___\\\\  \\|\\___   ___\\\\   __  \\|\\   ___  \\|\\  \\|\\   ____\\ ");
+        Console.WriteLine("       \\|___ \\  \\_\\ \\  \\|___ \\  \\_\\ \\  \\|\\  \\ \\  \\\\ \\  \\ \\  \\ \\  \\___|");
+        Console.WriteLine("            \\ \\  \\ \\ \\  \\   \\ \\  \\ \\ \\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\ \\  \\____ ");
+        Console.WriteLine("             \\ \\__\\ \\ \\__\\   \\ \\__\\ \\ \\__\\ \\__\\ \\__\\\\ \\__\\ \\__\\ \\_______\\       ⢢⣿⢻ ");
+        Console.WriteLine("              \\|__|  \\|__|    \\|__|  \\|__|\\|__|\\|__| \\|__|\\|__|\\|_______|       ⢼⡯⡨⠀⠀⣺⡿⠃");
+        Console.WriteLine("                 ⠀                                                          ⢈⣀⣌⡀⣼⡓⠆⢀⡀⣿⢂⠁ ⢨⡟⡃ ⢰⡦");
+        Console.WriteLine("                                                            ⢸⢂⡮⣴⢤⣖⣀⣆⢠⡀⡄⡁⣀⢷⠳⡷⡭⡫⠮⡱⡮⣟⣛⠻⠣⣭⡢⢤⡘⡣⠀⡿⡁⡽⠃⠀⠨⠀⠀⠀⠀⠄⠀⠀");
+        Console.WriteLine("                                                            ⣸⣿⣿⣿⣿⣷⣿⣾⣷⣯⣮⣽⣬⣩⣃⣋⡚⢙⠍⠳⠽⠍⠉⠛⠃⠢⠌⢩⢑⠊⠝⠤⢂⡝⡠⢄⡐⠀⠀⠀⡀⠀⠀⠁");
+        Console.WriteLine("                                                            ⢛⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣦⣦⣴⣤⣀⣄⣀⡈⡈⠈⠐⠐⠈⠄⢈⠈⠡⠀⠀⠀⠐⠀");
+        Console.WriteLine("                                                            ⢨⣿⣿⢿⣯⣿⣽⣷⣿⣟⣿⣻⣿⣻⣿⡿⣿⣿⣻⡿⣿⢿⣿⣻⣯⣿⣻⢿⣝⣿⣻⣟⢿⣛⣟⢞⣗⠝⠁⡀⡀⢂⠐⠀");
+        Console.WriteLine("                                                            ⢑⣿⣻⣟⣿⣻⣻⢯⣟⣿⡻⣟⢿⡫⢟⡛⢿⠾⡻⡟⢿⢻⢏⡿⡳⡫⡥⢕⢆⡥⣨⡉⣁⢁⠅⠣⠐⢌⠒⡀⢊⢄⢑⠑");
+        Console.WriteLine("                                                              ⢵⡮⣾⢵⢣⡏⣵⡵⣬⣬⣡⡣⣝⣇⢟⣝⢛⢞⢜⣑⢱⢢⠪⢎⠾⡘⣧⢲⡬⣮⡺⣜⢭⢫⡛⡺⢌⢏⡺⡱⡣⣮⣅");
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("          원하시는 캐릭터의 이름을 설정해주세요.");
+        Console.Write("          >> ");
         string name = Console.ReadLine();
 
         Console.Clear();
+        Console.WriteLine();
+        Console.WriteLine();
         Console.WriteLine($"당신의 캐릭터 [{name}]의 직업을 설정해주세요.");
+        Console.WriteLine();
         Console.WriteLine("1. 워리어");
         Console.WriteLine("2. 시프");
         while (true)
