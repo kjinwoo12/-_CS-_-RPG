ba44f5a5b50c4250382a59627658c3e3578752f1
Kim JinWoo <kjinwoo12@gmail.com>
2024-10-01 21:44:40 +0900

    refactor: Skill System

---
+++ Combat/Skills/Skill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+
+// 스킬을 관리하는 클래스
+public abstract class Skill : TargetingValidator
+{
+    public Character owner { get; private set; }
+    public string skillName { get; set; }
+    public string skillDescription { get; set; }
+
+    public Skill(Character owner, string name, string description)
+    {
+        this.owner = owner;
+        skillName = name;
+        skillDescription = description;
+    }
+
+    public abstract void OnUse(List<Character> character);
+}
